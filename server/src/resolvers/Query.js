239bfc357be57d64ddb798c9ae7ae7bbeaf2c3fc
xeusteerapat <xeus085@gmail.com>
2020-05-17 00:18:48 +0700

    update query items

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,14 +1,8 @@
 const Query = {
-  // item: (parent, args, ctx, info) => {
-  //   return [
-  //     {
-  //       name: 'Brown'
-  //     },
-  //     {
-  //       name: 'Blue'
-  //     }
-  //   ];
-  // }
+  items: async (parent, args, ctx, info) => {
+    const items = await ctx.db.query.items();
+    return items;
+  }
 };
 
 module.exports = Query;
