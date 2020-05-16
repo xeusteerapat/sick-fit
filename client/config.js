3c33e6339e8dc791d63fff6a3ba5d1e152626924
xeusteerapat <xeus085@gmail.com>
2020-05-17 00:39:53 +0700

    setup apollo client

---
+++ client/config.js
@@ -0,0 +1,3 @@
+// This is client side config only
+export const endpoint = `http://localhost:4444`;
+export const perPage = 4;
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import App from 'next/app';
+import { ApolloProvider } from 'react-apollo';
+import withApollo from '../lib/withApollo';
 import Page from '../components/Page';
 
 class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    pageProps.query = ctx.query;
+    return { pageProps };
+  }
+
   render() {
-    const { Component, pageProps } = this.props;
+    const { Component, pageProps, apollo } = this.props;
     return (
-      <Page>
-        <Component {...pageProps} name="teerapat" />
-      </Page>
+      <ApolloProvider client={apollo}>
+        <Page>
+          <Component {...pageProps} />
+        </Page>
+      </ApolloProvider>
     );
   }
 }
 
-export default MyApp;
+export default withApollo(MyApp);
