4406dcd9596830f2ffa75a85c1bcc22f07271e3e
xeusteerapat <xeus085@gmail.com>
2020-05-24 23:33:10 +0700

    update toggle Cart component

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 import NavStyles from './styles/NavStyles';
+import { Mutation } from 'react-apollo';
+import { TOGGLE_CART_MUTATION } from './Cart';
 import User from './User';
 import Signout from './Signout';
 
@@ -28,6 +30,9 @@ const Nav = () => {
                   <a>Account</a>
                 </Link>
                 <Signout />
+                <Mutation mutation={TOGGLE_CART_MUTATION}>
+                  {toggleCart => <button onClick={toggleCart}>My Cart</button>}
+                </Mutation>
               </>
             )}
             {!data.me && (
